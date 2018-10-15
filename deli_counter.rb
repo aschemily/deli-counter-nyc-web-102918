51bda3d7e0060ae3a68d2eed05f73abb5de710b4
Emily Asch <e.asch@me.com>
2018-10-15 15:49:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,19 @@ def line(katz_deli)
     puts "The line is currently: #{line_position.join(" ")}"
   end
 end
+
+def take_a_number(katz_deli,name)
+  katz_deli << name 
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end
+end
+end
